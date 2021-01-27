669cb7001ca8a7e3942858ed807fcd27c202c47e
hamza_ch <mhamzasulehri143@gmail.com>
2021-01-27 12:23:23 +0500

    drawing file rename

--- js/dowload_functions.js
+++ js/dowload_functions.js
@@ -2,21 +2,21 @@ function download_as_png(){
     const a=document.createElement("a");
     document.body.appendChild(a);
     a.href=canvas_container.toDataURL('image/png');
-    a.download="untitle.png";
+    a.download="drawing.png";
     a.click();
     document.body.removeChild("a");
 }
 function download_as_jpg(){
-    const a=document.createElement("a");
-    document.body.appendChild(a);
-    a.href=canvas_container.toDataURL('image/jpg');
-    a.download="untitle.jpg";
-    a.click();
-    document.body.removeChild("a");
+        const a=document.createElement("a");
+        document.body.appendChild(a);
+        a.href = canvas_container.toDataURL('image/jpg');
+        a.download = "drawing.jpg";
+        a.click();
+        document.body.removeChild("a");
 }
 function download_as_pdf(){
       var imgData = canvas_container.toDataURL("image/jpeg", 1.0);
         var pdf = new jsPDF();
         pdf.addImage(imgData, 'JPEG', 0, 0);
-        pdf.save("untitle.pdf");
+        pdf.save("drawing.pdf");
 }
