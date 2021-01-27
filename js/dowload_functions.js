f5fdf0887b78f261ea3f9b28575b93cb20310042
hamza_ch <mhamzasulehri143@gmail.com>
2021-01-27 12:25:56 +0500

    drawing pdf download changes

--- js/dowload_functions.js
+++ js/dowload_functions.js
@@ -7,16 +7,16 @@ function download_as_png(){
     document.body.removeChild("a");
 }
 function download_as_jpg(){
-        const a=document.createElement("a");
-        document.body.appendChild(a);
-        a.href = canvas_container.toDataURL('image/jpg');
-        a.download = "drawing.jpg";
-        a.click();
-        document.body.removeChild("a");
+    const a=document.createElement("a");
+    document.body.appendChild(a);
+    a.href = canvas_container.toDataURL('image/jpg');
+    a.download = "drawing.jpg";
+    a.click();
+    document.body.removeChild("a");
 }
 function download_as_pdf(){
-      var imgData = canvas_container.toDataURL("image/jpeg", 1.0);
-        var pdf = new jsPDF();
-        pdf.addImage(imgData, 'JPEG', 0, 0);
-        pdf.save("drawing.pdf");
+    let img = canvas_container.toDataURL('image/jpeg',1.0);
+    let pdf = new jsPDF();
+    pdf.addImage(img,'JPEF',0,0);
+    pdf.save("drawing.pdf"); 
 }
